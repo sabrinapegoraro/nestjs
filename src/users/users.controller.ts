1c4353ec6206579b88dea44a6418a62e2e217667
sabrinapegoraro <sabrinapegoraro06@gmail.com>
2024-10-07 14:46:45 -0300

    criação do método GET e função findAll

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    index():User[] {
+        return this.usersService.findAll()
+    }
 
     @Post()
     create(@Body() user: User): User {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,10 @@ import { User } from './interfaces/user.interface';
 export class UsersService {
     private users: User[] = [];
 
+    findAll(): User[]{
+        return this.users;
+    }
+
     create(user: User): User{
         this.users.push(user);
         return user;
