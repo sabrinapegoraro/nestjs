56458268b1e5fffbad12f8f299adc04488081f28
sabrinapegoraro <sabrinapegoraro06@gmail.com>
2024-10-07 14:41:47 -0300

    criação método POST e função create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    create(@Body() user: User): User {
+        return this.usersService.create(user);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [];
+
+    create(user: User): User{
+        this.users.push(user);
+        return user;
+    }
+}
