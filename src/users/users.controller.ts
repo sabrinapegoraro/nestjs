c1d182a6bbf85fb1f17aedaf9fe18101cbb5852f
Sabrina Pegoraro <sabrina757802@gmail.com>
2024-10-07 22:23:22 -0300

    repository para persisitir dados

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,19 +3,20 @@ import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 import { ApiBody } from '@nestjs/swagger';
 import { UserDto } from './dtos/user.dtos';
+import { UserEntity } from './database/user.entity';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get()
-    index():User[] {
-        return this.usersService.findAll()
+    async index(): Promise<UserEntity[]> {
+        return await this.usersService.findAll();
     }
 
     @Post()
     @ApiBody({ type: UserDto })
-    create(@Body() user: UserDto): User {
-        return this.usersService.create(user);
+    async create(@Body() user: UserDto): Promise<UserEntity> {
+        return await this.usersService.create(user);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './interfaces/user.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './database/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    private users: User[] = [];
+    constructor(@InjectRepository(UserEntity) 
+    private userRepository: Repository<UserEntity>
+    ) {}
 
-    findAll(): User[]{
-        return this.users;
+    async findAll(): Promise<UserEntity[]>{
+        return await this.userRepository.find();
     }
 
-    create(user: User): User{
-        this.users.push(user);
-        return user;
+    async create(user: User): Promise<UserEntity>{
+        return await this.userRepository.save(user);
     }
 }
